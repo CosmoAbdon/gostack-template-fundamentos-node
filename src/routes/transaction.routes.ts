e8a820c9565f7e7e7b281d2caf1c854bf8833767
Cosmo Abdon <cosmoabdon@2bebot.com>
2020-08-13 08:13:21 -0300

    styling

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -8,26 +8,26 @@ const transactionRouter = Router();
 const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
-
   try {
     const transactions = transactionsRepository.all();
     const balance = transactionsRepository.getBalance();
 
-    return response.json({transactions, balance});
+    return response.json({ transactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
 
 transactionRouter.post('/', (request, response) => {
-  const {title, value, type} = request.body
+  const { title, value, type } = request.body;
   try {
-    const createTransaction = new CreateTransactionService(transactionsRepository);
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
 
-    const transaction = createTransaction.execute({title, value, type});
+    const transaction = createTransaction.execute({ title, value, type });
 
     return response.json(transaction);
-
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
